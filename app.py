857c6d07969646aa5dc39fde383a991779c90b2d
NarendraTekale <145116967+NarendraTekale@users.noreply.github.com>
2025-08-14 15:42:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -107,5 +107,9 @@ def index():
     
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+def health_check():
+    return "OK", 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
